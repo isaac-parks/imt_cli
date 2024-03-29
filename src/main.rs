2a66dee53ca68ffe987f1b409a8c391a98bb5f3b
Isaac Parks <isaac.parks@imtins.com>
2024-03-29 02:28:57 -0500

    working version

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::io::Error;
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::{SystemTime, Duration, UNIX_EPOCH};
 use env::VarError;
 
 use std::process::Command;
@@ -8,6 +8,7 @@ use std::process::Output;
 use std::path::Path;
 use std::fs::read_dir;
 use std::fs::FileType;
+use std::str::FromStr;
 
 #[derive(PartialEq)]
 enum Action {
@@ -16,57 +17,77 @@ enum Action {
 }
 
 fn install() {
-// TOOD
+ println("Jk, this isn't implemented yet.");
 }
 
-fn prune() {
-    let service_dir: String = make_dir(env::var("IMT_SERVICES_DIR"));
-    if !set_working_dir(&service_dir) {
-        println!("ERROR: Couldn't set working directory");
-    }
-    let root_dir = env::current_dir().unwrap();
-    let root_dir_path = root_dir.as_path().to_str().unwrap();
-    let files = read_dir(&root_dir);
-    if let Ok(file_list) = files {
-        for file in file_list {
-            match &file {
-                Ok(dir_entry) => {
-                    if !dir_entry.file_type().unwrap().is_dir() {
-                        continue; // skip non dir
+fn rm_old_branches_from_cd(branch_data: &Vec<(String, usize)>) -> usize {
+    println!("Deleting old branches from {:?}", env::current_dir().unwrap());
+    let sys_t = SystemTime::now();
+    let now = sys_t.duration_since(UNIX_EPOCH).unwrap();
+    let one_twenty_days = Duration::from_secs(10368000);
+    let cut_off = now - one_twenty_days;
+    let ignore: [&str; 4] = ["staging", "production", "qa", "master"];
+    let mut deleted_num = 0;
+    for data in branch_data {
+        let last_commit_date = Duration::from_secs(data.1.try_into().unwrap());
+        if last_commit_date < cut_off {
+            let branch_name = &data.0.as_str();
+            if !ignore.contains(branch_name) {
+                let mut delete_cmd = Command::new("git");
+                delete_cmd.args(["branch", "-D", branch_name]);
+                dbg!(&delete_cmd);
+                match delete_cmd.output() {
+                    Ok(output) => {
+                        println!("Attempting to delete branch \"{}\".", data.0);
+                        let stdout = std::str::from_utf8(&output.stdout).unwrap_or("Could not decode stdout");
+                        let stderr = std::str::from_utf8(&output.stderr).unwrap_or("Could not decode stderr");
+                        println!("Command output (if any): {}", stdout);
+                        println!("Command error (if any): {}", stderr);
+                        deleted_num += 1;
                     }
-                    set_working_dir(&String::from(dir_entry.path().to_str().unwrap()));
-                    // make tuple vec, each branch name, last commit date
-                    // todo get all branches
-                    // compare date to current date
-                    // if too old delete
-                    get_cd_last_commit_dates();
-                    set_working_dir(&String::from(root_dir_path));
-                },
-                _ => ()
+                    Err(_) => {
+                        println!("Couldn't delete branch \"{}\".", data.0);
+                    }
+                }
             }
         }
     }
+    println!("Deleted {} branches from {:?}🪦", deleted_num, env::current_dir().unwrap());
+    deleted_num
 }
 
-fn get_cd_last_commit_dates() {
-    // let branch_commit_dates = Vec::new();
+fn get_cd_last_commit_dates() -> Vec<(String, usize)> {
+    let mut branch_commit_dates: Vec<(String, usize)> = Vec::new();
     let mut branch_cmd = Command::new("git");
-    branch_cmd.args(["for-each-ref", &format!("--format=%%(refname:short)|%%(committerdate:unix)"), "refs/heads"]);
+    branch_cmd.args(["for-each-ref", "--format=%(refname:short)|%(committerdate:unix)", "refs/heads"]);
     let output = branch_cmd.output();
-    match &output {
+    match output {
         Ok(out) => {
-            println!("{:?}", out.stdout);
+            let p = "\n";
+            let utf8 = String::from_utf8(out.stdout).unwrap();
+            let mut split: &Vec<&str> = &utf8.split(&p).collect();
+            for branch_split in split {
+                let name_date_split: Vec<&str> = branch_split.split('|').collect();
+                if name_date_split.len() != 2 {
+                    continue;
+                }
+                let name: &str = name_date_split[0];
+                let unix_ts_str: &str = name_date_split[1];
+            
+                match unix_ts_str.parse::<usize>() {
+                    Ok(unix_ts) => {
+                        branch_commit_dates.push((String::from(name), unix_ts.try_into().unwrap()));
+                    },
+                    Err(e) => {
+                        eprintln!("Failed to parse timestamp for '{}': {}", name, e);
+                    }
+                }
+            }
         },
         _ => ()
     }
-
+    branch_commit_dates
 }
-
-// fn rm_old_branch() {
-//     let mut branch_cmd = Command::new("git");
-//     git_cmd.args(["fetch", "--all", "--prune"]);
-    
-// }
 
 fn set_working_dir(service_dir: &String) -> bool {
     let root = Path::new(&service_dir);
@@ -86,6 +107,37 @@ fn make_dir(service_dir: Result<String,VarError>) -> String{
         }
     }
     String::new()
+}
+
+fn prune() -> usize {
+    let service_dir: String = make_dir(env::var("IMT_SERVICES_DIR"));
+    if !set_working_dir(&service_dir) {
+        println!("ERROR: Couldn't set working directory");
+    }
+    let root_dir = env::current_dir().unwrap();
+    let root_dir_path = root_dir.as_path().to_str().unwrap();
+    let files = read_dir(&root_dir);
+    let mut total_del: usize = 0;
+    if let Ok(file_list) = files {
+        for file in file_list {
+            match &file {
+                Ok(dir_entry) => {
+                    if !dir_entry.file_type().unwrap().is_dir() {
+                        continue;
+                    }
+                    set_working_dir(&String::from(dir_entry.path().to_str().unwrap()));
+                    let branch_tups: Vec<(String, usize)> = get_cd_last_commit_dates();
+                    let num_deleted: usize = rm_old_branches_from_cd(&branch_tups);
+                    total_del += num_deleted;
+                    get_cd_last_commit_dates();
+                    set_working_dir(&String::from(root_dir_path));
+                },
+                _ => ()
+            }
+        }
+    }
+
+    total_del
 }
 
 fn parse_args(args: &mut Vec<String>) -> Vec<Action> {
@@ -108,7 +160,10 @@ fn main() {
     for action in actions {
         match action {
             Action::Install => install(),
-            Action::Prune => prune()
+            Action::Prune => {
+                let total_del: usize = prune();
+                println!("Done pruning. Deleted a total of {} branches.", total_del);
+            }
         }
     }
 }
